a4cdc4810f34b5f6ec6d21c583717a595f7e7bed
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2024-04-05 06:49:58 -0700

    Added a floating action button for the nav manu

---
+++ src/components/CharacterSheet.vue
@@ -0,0 +1,112 @@
+<template>
+  <ion-grid fixed>
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="6" size-lg="5" size-xs="12">
+        <character-bio></character-bio>
+      </ion-col>
+    </ion-row>
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="6" size-lg="5" size-xs="12">
+        <experience-bar :totalXP=20 :spentXP=20 :startingXP=40 :availableXP=40></experience-bar>
+      </ion-col>
+    </ion-row>  
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="6" size-lg="5" size-xs="12">
+        <stat-group/>
+      </ion-col>
+    </ion-row>  
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="2.5">
+          <attribute-score name="brutality" :score=2></attribute-score>
+      </ion-col>
+      <ion-col size-md="2.5">
+          <attribute-score name="cunning" :score=2></attribute-score>
+      </ion-col>
+    </ion-row>
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="2.5">
+          <attribute-score name="wisdom" :score=2></attribute-score>
+      </ion-col>
+      <ion-col size-md="2.5">
+          <attribute-score name="intellect" :score=2></attribute-score>
+      </ion-col>
+    </ion-row>
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="2.5">
+          <attribute-score name="passion" :score=2></attribute-score>
+      </ion-col>
+      <ion-col size-md="2.5">
+          <attribute-score name="empathy" :score=2></attribute-score>
+      </ion-col>
+    </ion-row>
+    <ion-row class="ion-justify-content-center">
+      <ion-col size-md="6" size-lg="5" size-xs="12">
+        <ion-accordion-group ref="accordionGroup" :multiple="true" :value="getAttributes">
+          <ion-accordion v-for="attribute in getAttributes" :key="attribute" :value="attribute">
+            <ion-item slot="header" color="light" class="attribute-header">
+              <ion-label>{{ attribute }}</ion-label>
+            </ion-item>
+            <div class="ion-padding" slot="content">
+              <skill-score 
+                v-for="skill in skills.filter(s => s.attribute === attribute)" 
+                :key="skill.name" 
+                :name="skill.name" 
+                :type="skill.rank.toLowerCase()" 
+                :rank="getCharacterSkillRank(currentCharacter, skill.name)"/>
+            </div>
+          </ion-accordion>
+        </ion-accordion-group>   
+      </ion-col>  
+    </ion-row> 
+  </ion-grid>
+</template>
+
+<script setup lang="ts">
+import {watch} from 'vue';
+
+import { IonGrid, IonRow, IonCol, IonItem, IonLabel, IonAccordionGroup, IonAccordion, loadingController } from '@ionic/vue';
+import AttributeScore from "../components/AttributeScore.vue";
+import StatGroup from "../components/StatGroup.vue";
+import SkillScore from "../components/SkillScore.vue";
+import CharacterBio from "../components/CharacterBio.vue";
+import ExperienceBar from "../components/ExperienceBar.vue";
+
+import { useGameStore } from "../stores/game";
+import { useCharacterStore } from "../stores/character";
+import { storeToRefs } from 'pinia'
+const { skills, getAttributes, loading } = storeToRefs(useGameStore());
+const { currentCharacter, getCharacterSkillRank } = storeToRefs(useCharacterStore());
+
+(async () => {
+  const loader = await loadingController.create({
+    message: 'Loading...',
+  });
+  watch(loading, async (newValue, oldValue) => {
+    if (newValue) loader.present();
+    else loader.dismiss();
+  });
+  if (loading.value) loader.present();
+})();
+
+const { fetchSkills } = useGameStore()
+fetchSkills();
+</script>
+
+<style scoped>
+.attribute-header {
+  margin-bottom: 0.2em;
+  margin-top: 0.2em;
+  border-radius: 1em;
+}
+
+ion-card-content p {
+  font-size: 1em;
+  color: #666; /* Subtle color for the description */
+}
+
+.ion-padding {
+  display: flex;
+  flex-direction: column;
+  gap: 0.25em;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,115 +1,30 @@
 <template>
-  <ion-page>
+  <ion-menu type="push" menu-id="sidebar" content-id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Menu Content</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">This is the menu content.</ion-content>
+  </ion-menu>
+  <ion-page id="main-content">   
     <ion-content :fullscreen="true">
-      <ion-grid fixed>
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="6" size-lg="5" size-xs="12">
-            <character-bio></character-bio>
-          </ion-col>
-        </ion-row>
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="6" size-lg="5" size-xs="12">
-            <experience-bar :totalXP=20 :spentXP=20 :startingXP=40 :availableXP=40></experience-bar>
-          </ion-col>
-        </ion-row>  
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="6" size-lg="5" size-xs="12">
-            <stat-group/>
-          </ion-col>
-        </ion-row>  
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="2.5">
-             <attribute-score name="brutality" :score=2></attribute-score>
-          </ion-col>
-          <ion-col size-md="2.5">
-             <attribute-score name="cunning" :score=2></attribute-score>
-          </ion-col>
-        </ion-row>
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="2.5">
-             <attribute-score name="wisdom" :score=2></attribute-score>
-          </ion-col>
-          <ion-col size-md="2.5">
-             <attribute-score name="intellect" :score=2></attribute-score>
-          </ion-col>
-        </ion-row>
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="2.5">
-             <attribute-score name="passion" :score=2></attribute-score>
-          </ion-col>
-          <ion-col size-md="2.5">
-             <attribute-score name="empathy" :score=2></attribute-score>
-          </ion-col>
-        </ion-row>
-        <ion-row class="ion-justify-content-center">
-          <ion-col size-md="6" size-lg="5" size-xs="12">
-            <ion-accordion-group ref="accordionGroup" :multiple="true" :value="getAttributes">
-              <ion-accordion v-for="attribute in getAttributes" :key="attribute" :value="attribute">
-                <ion-item slot="header" color="light" class="attribute-header">
-                  <ion-label>{{ attribute }}</ion-label>
-                </ion-item>
-                <div class="ion-padding" slot="content">
-                  <skill-score 
-                    v-for="skill in skills.filter(s => s.attribute === attribute)" 
-                    :key="skill.name" 
-                    :name="skill.name" 
-                    :type="skill.rank.toLowerCase()" 
-                    :rank="getCharacterSkillRank(currentCharacter, skill.name)"/>
-                </div>
-              </ion-accordion>
-            </ion-accordion-group>   
-          </ion-col>  
-        </ion-row> 
-      </ion-grid>
+      <ion-fab slot="fixed" vertical="top" horizontal="start">
+        <ion-fab-button @click="menuController.open('sidebar')">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <character-sheet />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import {watch} from 'vue';
-import { IonCardTitle, IonCardContent, IonCard, IonPage, IonGrid, IonRow, IonCol, IonItem, IonLabel, IonAccordionGroup, IonAccordion, IonContent, loadingController } from '@ionic/vue';
-import AttributeScore from "../components/AttributeScore.vue";
-import StatGroup from "../components/StatGroup.vue";
-import SkillScore from "../components/SkillScore.vue";
-import CharacterBio from "../components/CharacterBio.vue";
-import ExperienceBar from "../components/ExperienceBar.vue";
-
-import { useGameStore } from "../stores/game";
-import { useCharacterStore } from "../stores/character";
-import { storeToRefs } from 'pinia'
-const { skills, getAttributes, loading } = storeToRefs(useGameStore());
-const { currentCharacter, getCharacterSkillRank } = storeToRefs(useCharacterStore());
-
-(async () => {
-  const loader = await loadingController.create({
-    message: 'Loading...',
-  });
-  watch(loading, async (newValue, oldValue) => {
-    if (newValue) loader.present();
-    else loader.dismiss();
-  });
-  if (loading.value) loader.present();
-})();
-
-const { fetchSkills } = useGameStore()
-fetchSkills();
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonFab, IonFabButton, IonIcon, IonContent, IonPage, menuController  } from '@ionic/vue';
+import { add } from 'ionicons/icons';
+import CharacterSheet from '@/components/CharacterSheet.vue';
 </script>
 
 <style scoped>
-.attribute-header {
-  margin-bottom: 0.2em;
-  margin-top: 0.2em;
-  border-radius: 1em;
-}
-
-ion-card-content p {
-  font-size: 1em;
-  color: #666; /* Subtle color for the description */
-}
-
-.ion-padding {
-  display: flex;
-  flex-direction: column;
-  gap: 0.25em;
-}
 </style>
